2b992dfeab7c9519721315b532e118641768bf08
toitenquang <quangbm.bi12-373@st.usth.edu.vn>
2023-05-31 16:38:41 +0700

    labwork6

---
+++ pw6.py/main.py
@@ -0,0 +1,38 @@
+from output import *
+
+def main():
+    smm = Output()
+    smm.input_student_information()
+    smm.input_course_information()
+
+    while True:
+        print("\n1. Input student marks")
+        print("2. List students")
+        print("3. List courses")
+        print("4. Show student marks")
+        print("5. Display gpa")
+        print("6. Sort students gpa")
+        print("7. Compress")
+        print("8. Decompress")
+        print("9. Exit")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            smm.input_student_marks()
+        elif choice == 2:
+            smm.list_students()
+        elif choice == 3:
+            smm.list_courses()
+        elif choice == 4:
+            smm.show_student_marks()
+        elif choice == 5:
+            smm.calculate_gpa()
+        elif choice == 6:
+            smm.sort_student_by_gpa()
+        elif choice == 7:
+            Save.Compress()
+        elif choice == 8:
+            Open.decompress()
+        elif choice == 9:    
+            break
+if __name__ == "__main__":
+    main()
---
+++ pw6.py/output.py
@@ -0,0 +1,58 @@
+import numpy as np
+import math
+from input import *
+
+class Output(Input):
+    def __init__(self,id = str(),name = str(),dob = str(),cid = str(),cname = str(),credits = int(),mark = None):
+        input.__init__(self,id,name,dob,cid,cname,credits,mark)
+        self.list_gpa = []
+    def display_students_info(self):
+        for student in self.stu_list:
+            print("studetn id: ",student[0])
+            print("studetn name: ",student[1])
+            print("date of birth: ",student[2])
+
+    def display_course_info(self):
+        for course in self.course_list:
+            print("course id: ",course[0])
+            print("course name: ",course[1])
+            print("this course has numbers of credits: ",course[2])
+
+    def get_mark(self,stu_name,course_name):
+        return self.mark_dict.get((stu_name,course_name),None)
+    def display_marks(self):
+        course_name = input("Enter the course name you want to display marks: ")
+        if course_name not in self.list_course_name:
+            print("No course was found")
+            return 0
+        else:
+            print(f"Marks for {course_name}")
+            for self.stu_name in self.list_student_name:
+                mark = self.get_mark(self.stu_name,course_name)
+                print(f"{self.stu_name} : {mark}")
+
+    def calculate_gpa(self):
+        credits_array = np.array([list_credits])
+        for self.stu_name in list_student_name:
+            product_array = np.array([])
+            for i in range(len(list_course_name)):
+                course_name = list_course_name[i]
+                mark = self.marks.get_marks(self.stu_name, course_name)
+                if mark is None:
+                    print(f"No mark found for {self.su_name}")
+                    continue
+                product = mark * list_credits[i]
+                product_array = np.append(product_array, product)
+            total_credits = np.sum(credits_array)
+            if len(product_array) > 0:
+                gpa = round(np.sum(product_array ) / total_credits,2)
+                self.list_gpa.append(gpa)
+                print(f"Gpa for {self.stu_name} : {gpa}")
+
+    def sort_student_by_gpa(self):
+        self.calculate_gpa()
+        gpa_dict = dict(zip(list_student_name, self.list_gpa))
+        sorted_gpa_dict = dict(sorted(gpa_dict.items(), key=lambda x: x[1], reverse=True))
+        print("After students's name are sorted by GPA (descencing):")
+        for student, gpa in sorted_gpa_dict.items():
+            print(f"GPA of {student} : {gpa}")
